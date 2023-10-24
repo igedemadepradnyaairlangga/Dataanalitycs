4a09d0cbcaf96fffd79e4a9b32eadc4f341bf29c
igedemadepradnyaairlangga <72331276+igedemadepradnyaairlangga@users.noreply.github.com>
2023-10-24 09:54:46 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
@@ -22,7 +21,7 @@ if data is not None:
 
     # Analisis faktor-faktor berpengaruh
     st.header('Analisis Faktor-Faktor')
-    
+
     # Pilih variabel independen dan dependen
     X = df[['temp', 'registered', 'casual', 'instant']]
     y = df['cnt']
@@ -40,19 +39,16 @@ if data is not None:
     st.write('registered:', model.coef_[1])
     st.write('casual:', model.coef_[2])
     st.write('instant:', model.coef_[3])
-    # Konversi kolom 'tanggal' ke tipe data datetime
-    df['dteday'] = pd.to_datetime(df['dteday'])
-
-    # Set kolom 'tanggal' sebagai index
-    df.set_index('dteday', inplace=True)
-
-    # Resample data harian
-    daily_data = df['cnt'].resample('D').sum()
 
     # Eksplorasi tren harian
     st.title("Eksplorasi Tren Harian Penggunaan Sepeda")
 
-    # Plot tren harian
+    # Resample data harian
+    df['dteday'] = pd.to_datetime(df['dteday'])
+    df.set_index('dteday', inplace=True)
+    daily_data = df['cnt'].resample('D').sum()
+
+    # Plot tren harian menggunakan st.line_chart
     st.write("Grafik Tren Harian:")
     st.line_chart(daily_data, use_container_width=True)
 
@@ -68,45 +64,31 @@ if data is not None:
     # Anda juga dapat mencoba heatmap untuk mengidentifikasi pola musiman berdasarkan hari dalam seminggu dan bulan dalam tahun
     df['weekday'] = df.index.day_name()
     df['mnth'] = df.index.month_name()
-
     heatmap_data = df.groupby(['weekday', 'mnth'])['cnt'].mean().unstack()
     st.write("Heatmap Tren Musiman:")
     st.write(heatmap_data.style.background_gradient(cmap='YlGnBu'))
 
     # Analisis dampak cuaca
     st.header('Analisis Dampak Cuaca')
-    
+
     # Visualisasi jumlah peminjaman berdasarkan suhu
-    plt.figure(figsize=(8, 6))
-    plt.scatter(df['temp'], df['cnt'])
-    plt.xlabel('temp')
-    plt.ylabel('Jumlah Peminjaman')
-    plt.title('Pengaruh Suhu terhadap Jumlah Peminjaman')
-    st.pyplot(plt)
+    st.write("Scatter Plot: Pengaruh Suhu terhadap Jumlah Peminjaman")
+    st.scatter_chart(df, x='temp', y='cnt')
 
     # Visualisasi jumlah peminjaman berdasarkan hujan
-    plt.figure(figsize=(8, 6))
-    df['hum_bin'] = df['hum'].apply(lambda x: 'Hujan' if x == 1 else 'Tidak Hujan')
-    df.groupby('hum_bin')['cnt'].mean().plot(kind='bar')
-    plt.xlabel('hum')
-    plt.ylabel('Rata-rata Jumlah Peminjaman')
-    plt.title('Pengaruh Hujan terhadap Rata-rata Jumlah Peminjaman')
-    st.pyplot(plt)
+    # Tambahkan kolom 'hum_bin' berdasarkan ambang batas
+    df['hum_bin'] = df['hum'].apply(lambda x: 'Hujan' if x > 0.5 else 'Tidak Hujan')
+    st.write("Bar Chart: Pengaruh Hujan terhadap Rata-rata Jumlah Peminjaman")
+    st.bar_chart(df.groupby('hum_bin')['cnt'].mean())
 
     # Visualisasi jumlah peminjaman berdasarkan kecepatan angin
-    plt.figure(figsize=(8, 6))
-    plt.scatter(df['windspeed'], df['cnt'])
-    plt.xlabel('windspeed')
-    plt.ylabel('Jumlah Peminjaman')
-    plt.title('Pengaruh Kecepatan Angin terhadap Jumlah Peminjaman')
-    st.pyplot(plt)
-
-    # Analisis perbedaan antara pengguna terdaftar dan kasual
-    st.header('Perbedaan Antara Pengguna Terdaftar dan Kasual')
+    st.write("Scatter Plot: Pengaruh Kecepatan Angin terhadap Jumlah Peminjaman")
+    st.scatter_chart(df, x='windspeed', y='cnt')
     
-    # Visualisasi perbedaan pengguna terdaftar dan kasual
-    df.groupby('casual')['registered'].mean().plot(kind='bar')
-    plt.xlabel('Tipe Pengguna')
-    plt.ylabel('Rata-rata Jumlah Peminjaman')
-    plt.title('Perbedaan Pola Peminjaman antara Pengguna Terdaftar dan Kasual')
-    st.pyplot(plt)
+   
+    # Analisis perbedaan antara pengguna terdaftar dan kasual
+    st.header('Perbandingan Antara Pengguna Terdaftar dan Kasual')
+
+    # Visualisasi perbandingan pengguna terdaftar dan kasual
+    perbandingan_data = df.groupby('casual')['registered'].mean()
+    st.line_chart(perbandingan_data)
